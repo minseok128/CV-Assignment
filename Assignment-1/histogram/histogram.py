35af8781c751dd21759939be7864a7fc94db2d65
michang <minseok128128@gmail.com>
2024-03-27 16:46:56 +0900

    Update histogram : implement using function

--- Assignment-1/histogram/histogram.py
+++ Assignment-1/histogram/histogram.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-image_name = "lena.png"
+image_name = "lena_gray.png"
 image_o = cv2.imread(image_name, cv2.IMREAD_GRAYSCALE)
 
 if image_o is None :
@@ -12,12 +12,11 @@ if image_o is None :
 image_e = cv2.equalizeHist(image_o)
 
 hist_o = cv2.calcHist([image_o], [0], None, [256], [0, 256])
-print(hist_o)
 hist_e = cv2.calcHist([image_e], [0], None, [256], [0, 256])
 
-plt.figure(figsize=(8, 8))
+plt.figure(figsize=(20, 10))
 plt.subplot(221), plt.imshow(image_o, 'gray'), plt.title('original')
 plt.subplot(222), plt.imshow(image_e, 'gray'), plt.title('equalized')
-plt.subplot(223), plt.plot(hist_o, range=(0, 255), bins=255), plt.title('equalized histogram')
-plt.subplot(224), plt.plot(hist_e, range=(0, 255), bins=255), plt.title('original histogram')
+plt.subplot(223), plt.bar(range(256), hist_o.ravel(), color='red'), plt.title('equalized histogram')
+plt.subplot(224), plt.bar(range(256), hist_e.ravel()), plt.title('original histogram')
 plt.show()
