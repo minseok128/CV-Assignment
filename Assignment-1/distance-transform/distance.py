226a70d7348beeec68ccb76fafdec97b74aa515b
michang <minseok128128@gmail.com>
2024-03-27 22:09:00 +0900

    Update distance : 각 거리 계산 구현

--- Assignment-1/distance-transform/distance.py
+++ Assignment-1/distance-transform/distance.py
@@ -1,17 +1,21 @@
 import cv2
 import copy
 import numpy as np
+import math
 from matplotlib import pyplot as plt
 
-# def city_block(image, i, j, forward):
-# 	i1, j1 = i - forward, j
-# 	i2, j2 = i, j - forward
-# 	if i1 < 0 or i1 >= HEIGHT or j1 < 0 or j1 >= WIDTH:
-# 		i1, j1 = i, j
-# 	if i2 < 0 or i2 >= HEIGHT or j2 < 0 or j2 >= WIDTH:
-# 		i2, j2 = i, j
-# 	return min(image[i][j], image[i1][j1] + 1, image[i2][j2] + 1)
-def city_block(image, i, j, forward):
+ROOT2 = math.sqrt(2)
+
+def euclidean(ij, ij1, ij2, ij3, ij4):
+	return min(ij, ij1 + 1.0, ij2 + ROOT2, ij3 + 1.0, ij4 + ROOT2)
+
+def city_block(ij, ij1, ij2, ij3, ij4):
+	return min(ij, ij1 + 1.0, ij2 + 2.0, ij3 + 1.0, ij4 + 2.0)
+
+def chess_board(ij, ij1, ij2, ij3, ij4):
+	return min(ij, ij1 + 1.0, ij2 + 1.0, ij3 + 1.0, ij4 + 1.0)
+
+def get_value(image, i, j, forward, func):
 	i1, j1 = i - forward, j
 	i2, j2 = i - forward, j - forward
 	i3, j3 = i, j - forward
@@ -24,35 +28,56 @@ def city_block(image, i, j, forward):
 		i3, j3 = i, j
 	if i4 < 0 or i4 >= HEIGHT or j4 < 0 or j4 >= WIDTH:
 		i4, j4 = i, j
-	return min(image[i][j], image[i1][j1] + 1, image[i2][j2] + 2, image[i3][j3] + 1, image[i4][j4] + 2)
+	return func(image[i][j], image[i1][j1], image[i2][j2], image[i3][j3], image[i4][j4])
 
 # 이미지 파일 이름 설정
-image_name = "distance.png"
+image_name = "distance-3.png"
 # 이미지를 그레이스케일로 읽기
 image_o = cv2.imread(image_name, cv2.IMREAD_GRAYSCALE)
 HEIGHT, WIDTH = image_o.shape[0], image_o.shape[1]
 print(WIDTH, HEIGHT)
 
-image_test = copy.deepcopy(image_o)
+image_euc = np.zeros_like(image_o, dtype='float64')
 for i in range(0, HEIGHT):
 	for j in range(0, WIDTH):
-		if image_o[i][j] > 0:
-			image_test[i][j] = 255
+		if image_o[i][j] == 255:
+			image_euc[i][j] = 255
 		else:
-			image_test[i][j] = 0
+			image_euc[i][j] = 0
+image_cty = copy.deepcopy(image_euc)
+image_chs = copy.deepcopy(image_euc)
 
 for i in range(0, HEIGHT):
 	for j in range(0, WIDTH):
-		image_test[i][j] = city_block(image_test, i, j, 1)
+		image_euc[i][j] = get_value(image_euc, i, j, 1, euclidean)
+		image_cty[i][j] = get_value(image_cty, i, j, 1, city_block)
+		image_chs[i][j] = get_value(image_chs, i, j, 1, chess_board)
 for i in range(HEIGHT - 1, -1, -1):
 	for j in range(WIDTH - 1, -1, -1):
-		image_test[i][j] = city_block(image_test, i, j, -1)
-cv2.normalize(image_test, image_test, 0, 255, cv2.NORM_MINMAX)
+		image_euc[i][j] = get_value(image_euc, i, j, -1, euclidean)
+		image_cty[i][j] = get_value(image_cty, i, j, -1, city_block)
+		image_chs[i][j] = get_value(image_chs, i, j, -1, chess_board)
+# for i in reversed(range(0, HEIGHT)):
+# 	for j in reversed(range(0, WIDTH)):
+# 		image_euc[i][j] = get_value(image_euc, i, j, 1, euclidean)
+# 		image_cty[i][j] = get_value(image_cty, i, j, 1, city_block)
+# 		image_chs[i][j] = get_value(image_chs, i, j, 1, chess_board)
+# for i in reversed(range(HEIGHT - 1, -1, -1)):
+# 	for j in reversed(range(WIDTH - 1, -1, -1)):
+# 		image_euc[i][j] = get_value(image_euc, i, j, -1, euclidean)
+# 		image_cty[i][j] = get_value(image_cty, i, j, -1, city_block)
+# 		image_chs[i][j] = get_value(image_chs, i, j, -1, chess_board)
 
+
+cv2.normalize(image_euc, image_euc, 0, 255, cv2.NORM_MINMAX)
+cv2.normalize(image_cty, image_cty, 0, 255, cv2.NORM_MINMAX)
+cv2.normalize(image_chs, image_chs, 0, 255, cv2.NORM_MINMAX)
 
 # 결과 표시
 plt.figure(figsize=(16, 10)), plt.subplots_adjust(hspace=0.4, wspace=0.4)
 plt.subplot(221), plt.imshow(image_o, 'gray'), plt.title('Original image')
-plt.subplot(222), plt.imshow(image_test, 'gray'), plt.title('Original image')
+plt.subplot(222), plt.imshow(image_euc, 'gray'), plt.title('Euclidean distance')
+plt.subplot(223), plt.imshow(image_cty, 'gray'), plt.title('City block distance')
+plt.subplot(224), plt.imshow(image_chs, 'gray'), plt.title('Chess board distance')
 plt.show()
 
